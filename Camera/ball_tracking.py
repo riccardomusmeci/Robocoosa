a2ac7d5a5126c44bc8c42d84e1c6f9246c4eb172
Carlo Nuccio <carlonuccio91@gmail.com>
2017-07-17 22:10:29 +0200

    Add files via upload

--- Camera/ball_tracking.py
+++ Camera/ball_tracking.py
@@ -1,4 +1,3 @@
-
 from collections import deque
 import numpy as np
 import math
@@ -6,26 +5,189 @@ import imutils
 import argparse
 import cv2
 import socket
+import time
+from object import Object
 
 ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
 args = vars(ap.parse_args())
 
 # connessione socket
-server_address = '192.168.1.58'
+server_address = '192.168.0.102'
 server_port = 1931
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((server_address, server_port))
+
+red = Object("red")
+blue = Object("blue")
+yellow = Object("yellow")
+green = Object("green")
 
 # distanza tra due punti -> serve a calcolare angolo
 def distance((x1, y1), (x2,y2)):
     dist = math.sqrt((math.fabs(x2-x1))**2+((math.fabs(y2-y1)))**2)
     return dist
 
+def findColor(color):
+	mask = cv2.inRange(hsv, color.lower, color.upper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	  
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
 
-# boundary HSV per colori, in questo caso solo rosso
-boundaries = [
-	((163, 100, 100), (178, 255, 255))
-]
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# only proceed if the radius meets a minimum size
+		if radius > 12:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			return True
+	
+def findObject(targetColor, othercolor):
+	mask = cv2.inRange(hsv, targetColor.lower, targetColor.upper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	  
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0 and findColor(othercolor):
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# only proceed if the radius meets a minimum size
+		if radius > 12:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2) # Cerchio
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)						#Centro
+			print "Found Object " + targetColor.type
+
+			pts.appendleft(center)
+
+			if center:
+				(center_x, center_y) = center
+				#quantifies the hypotenuse of the triangle
+				hypotenuse =  distance((center_x,center_y), (center_frame_x, center_frame_y))
+				#quantifies the horizontal of the triangle
+				horizontal = distance((center_x, center_y), (center_frame_x, center_y))
+				#makes the third-line of the triangle
+				thirdline = distance((center_frame_x, center_frame_y), (center_frame_x, center_y))
+				#calculates the angle using trigonometry
+				angle = np.arcsin((thirdline/hypotenuse))* 180/math.pi
+				#print 90 - angle
+				if center_x < center_frame_x:
+					msg = {
+						'ID' : 'Camera',
+						'differenza_angolo' : 90 - angle,
+						'verso_rotazione' : 0,
+						'target' : 'oggetto'
+					}
+					print msg
+					sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					sock.connect((server_address, server_port))
+					sock.sendall(str(msg))
+					sock.close()
+					#sock.sendall(str(msg))
+					#sock.close()
+					#cv2.putText(frame, str(int(angle)), (center_x-30, center_y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0,128,220), 2)
+				elif center_x > center_frame_x:
+					msg = {
+						'ID' : 'Camera',
+						'differenza_angolo' : 90 - angle,
+						'verso_rotazione' : 1,
+						'target' : 'oggetto'
+					}
+					print msg
+					sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					sock.connect((server_address, server_port))
+					sock.sendall(str(msg))
+					sock.close()
+
+def findArea(lower, upper, othercolor):
+	mask = cv2.inRange(hsv, targetColor.lower, targetColor.upper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	  
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0 and not findColor(othercolor):
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# only proceed if the radius meets a minimum size
+		if radius > 12:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2) # Cerchio
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)						#Centro
+			print "Found Area " + targetColor.type
+
+			pts.appendleft(center)
+
+			if center:
+				(center_x, center_y) = center
+				#quantifies the hypotenuse of the triangle
+				hypotenuse =  distance((center_x,center_y), (center_frame_x, center_frame_y))
+				#quantifies the horizontal of the triangle
+				horizontal = distance((center_x, center_y), (center_frame_x, center_y))
+				#makes the third-line of the triangle
+				thirdline = distance((center_frame_x, center_frame_y), (center_frame_x, center_y))
+				#calculates the angle using trigonometry
+				angle = np.arcsin((thirdline/hypotenuse))* 180/math.pi
+				#print 90 - angle
+				if center_x < center_frame_x:
+					msg = {
+						'ID' : 'Camera',
+						'differenza_angolo' : 90 - angle,
+						'verso_rotazione' : 0,
+						'target' : 'area'
+					}
+					print msg
+					sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					sock.connect((server_address, server_port))
+					sock.sendall(str(msg))
+					sock.close()
+					#cv2.putText(frame, str(int(angle)), (center_x-30, center_y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0,128,220), 2)
+				elif center_x > center_frame_x:
+					msg = {
+						'ID' : 'Camera',
+						'differenza_angolo' : 90 - angle,
+						'verso_rotazione' : 1,
+						'target' : 'area'
+					}
+					print msg
+					sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					sock.connect((server_address, server_port))
+					sock.sendall(str(msg))
+					sock.close()
+
 
 pts = deque(maxlen=args["buffer"])
 
@@ -34,6 +196,7 @@ pts = deque(maxlen=args["buffer"])
 camera = cv2.VideoCapture("rtsp://@192.168.0.101:554/live/ch00_0", cv2.CAP_FFMPEG)
 
 while True:
+	startTime = time.time()
 	# prendiamo i frame
 	(grabbed, frame) = camera.read()
 
@@ -42,91 +205,13 @@ while True:
 	height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
 	
 	# calcoliamo il centro del frame
-	(center_frame_x, center_frame_y) = (int(width / 2), int(height / 2))
+	(center_frame_x, center_frame_y) = (int(width / 2), height)
 
 	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	for (lower, upper) in boundaries:
-		mask = cv2.inRange(hsv, lower, upper)
-		mask = cv2.erode(mask, None, iterations=2)
-		mask = cv2.dilate(mask, None, iterations=2)
-
-		# find contours in the mask and initialize the current
-		# (x, y) center of the ball
-		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-			cv2.CHAIN_APPROX_SIMPLE)[-2]
-		center = None
-
-		# only proceed if at least one contour was found
-		if len(cnts) > 0:
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-			# only proceed if the radius meets a minimum size
-			if radius > 10:
-				# draw the circle and centroid on the frame,
-				# then update the list of tracked points
-				cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2) # Cerchio
-				cv2.circle(frame, center, 5, (0, 0, 255), -1)						#Centro
-
-		# update the points queue
-		pts.appendleft(center)
-
-		if center:
-			(center_x, center_y) = center
-			#quantifies the hypotenuse of the triangle
-			hypotenuse =  distance((center_x,center_y), (center_frame_x, center_frame_y))
-			#quantifies the horizontal of the triangle
-			horizontal = distance((center_x, center_y), (center_frame_x, center_y))
-			#makes the third-line of the triangle
-			thirdline = distance((center_frame_x, center_frame_y), (center_frame_x, center_y))
-			#calculates the angle using trigonometry
-			angle = np.arcsin((thirdline/hypotenuse))* 180/math.pi
-
-			if center_frame_y < center_y and center_frame_x > center_x:
-				sock.sendall(str(angle))
-    			#cv2.putText(frame, str(int(angle)), (center_x-30, center_y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0,128,220), 2)
-			elif center_frame_y < center_y and center_frame_x < center_x:
-				sock.sendall(str(180 - angle))
-				#cv2.putText(frame, str(int(180 - angle)),(center_x-30, center_y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0,128,220), 2)
-			elif center_frame_y > center_y and center_frame_x < center_x:
-				sock.sendall(str(180 + angle))
-				#cv2.putText(frame, str(int(180 + angle)),(center_x-30, center_y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0,128,220), 2)
-			elif center_frame_y > center_y and center_frame_x > center_x:
-				sock.sendall(str(360 - angle))
-				#cv2.putText(frame, str(int(360 - angle)),(center_x-30, center_y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0,128, 229), 2)
-
-			#draws all 3 lines
-			#cv2.line(frame, (center_x, center_y), (center_frame_x, center_frame_y), (0, 0, 255), 2)
-			#cv2.line(frame, (center_x, center_y), (center_frame_x, center_y), (0, 0, 255), 2)
-			#cv2.line(frame, (center_frame_x,center_frame_y), (center_frame_x, center_y), (0,0,255), 2)
-
-		
-		# 	#draws all 3 lines
-		# 	cv2.line(frame, center, (center_frame_x, center_frame_y), (0, 0, 255), 2)
-		# 	cv2.line(frame, center, (center_frame_x, center_y), (0, 0, 255), 2)
-		# 	cv2.line(frame, (center_frame_x,center_frame_y), (center_frame_x, center_y), (0,0,255), 2)
-
-		# loop over the set of tracked points
-		for i in xrange(1, len(pts)):
-			# if either of the tracked points are None, ignore
-			# them
-			if pts[i - 1] is None or pts[i] is None:
-				continue
-
-			# otherwise, compute the thickness of the line and
-			# draw the connecting lines
-			thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-			cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)  #traccia
+	findObject(red, blue)
+	
 
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
--- Camera/object.py
+++ Camera/object.py
@@ -2,35 +2,24 @@ import numpy as np
 
 class Object:
     def __init__(self, name = "Object"):
-        self.xPos = 0
-        self.yPos = 0
         self.type = name
 
         if name == "red":
-            self.HSVmin = np.array([163, 100, 100])
-            self.HSVmax = np.array([178, 255, 255])
-	        #BGR value for Red:
-            self.Color = (0,0,255)
+            self.lower = (163, 100, 100)
+            self.upper = (178, 255, 255)
 
         elif name == "yellow":
-            self.HSVmin = np.array([15, 100, 100])
-            self.HSVmax = np.array([42, 255, 255])
-	        #BGR value for Yellow:
-            self.Color = (0,255,255)
+            self.lower = (15, 100, 100)
+            self.upper = (42, 255, 255)
 
         elif name == "green":
-            self.HSVmin = np.array([62, 100, 47])
-            self.HSVmax = np.array([100, 255, 166])
-	        #BGR value for green:
-            self.Color = (0,255,0)
+            self.lower = (62, 100, 47)
+            self.upper = (100, 255, 166)
 
         elif name == "blue":
-            self.HSVmin = np.array([92, 100, 76])
-            self.HSVmax = np.array([125, 255, 255])
-	        #BGR value for Blue:
-            self.Color = (255,0,0)
+            self.lower = (92, 100, 76)
+            self.upper = (125, 255, 255)
 
         else:
-            self.HSVmin = np.array([0,0,0])
-            self.HSVmax = np.array([0,0,0])
-            self.Color = (0,0,0)
+            self.lower = (0,0,0)
+            self.upper = (0,0,0)
